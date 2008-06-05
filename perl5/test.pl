6835fe2c0ec23d50ed1c1c1cbd5b6826e967c9da
pfeller <>
2008-06-05 03:37:58 +0000

    SetWUnit and SetTUnit calls must now come after CreateOpen

--- perl5/test.pl
+++ perl5/test.pl
@@ -13,9 +13,6 @@ $service_time = 1.0;
 
 pdq::Init("OpenCenter");
 pdq::SetComment("A simple M/M/1 queue");
-   
-pdq::SetWUnit("Customers");
-pdq::SetTUnit("Seconds");
 
 
 #---- Define the queueing center ----------------------------------------------
@@ -26,6 +23,9 @@ $noNodes = pdq::CreateNode("server", $pdq::CEN, $pdq::FCFS);
 #---- Define the workload and circuit type ------------------------------------
 
 $noStreams = pdq::CreateOpen("work", $arrivRate);
+   
+pdq::SetWUnit("Customers");
+pdq::SetTUnit("Seconds");
 
 
 #---- Define service demand due to workload on the queueing center ------------
