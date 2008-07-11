360363b59159cd35fcc7f0051c3386a504170ec4
pfeller <>
2008-07-11 03:59:45 +0000

    Corrected the way that the Makefile is created on Solaris machines.

--- perl5/setup.sh
+++ perl5/setup.sh
@@ -3,13 +3,25 @@
 #  $Id$
 #
 
+if [ -f Makefile ]; then
 make clean
+fi
 
+if [ -f ../lib/libpdq.a ]; then
 ar xv ../lib/libpdq.a
+else
+cp ../lib/libpdq.so .
+fi
 
 cp ../lib/*.o .
 
+if [ `uname` = 'SunOS' ]; then
+perl Makefile.PL CC=gcc LD=gcc
+egrep -v '^(CCCDLFLAGS|OPTIMIZE)' Makefile > Makefile.new
+mv Makefile.new Makefile
+else
 perl Makefile.PL
+fi
 
 make install
 
