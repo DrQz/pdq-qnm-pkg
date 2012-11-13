c3ee4cdcf8c67afd1426d3864bf758848a899f65
earl-lang <>
2012-11-13 06:23:06 +0000

    Output as data frame and locate minimum RT row

--- R/pdq/demo/diskoptim.R
+++ R/pdq/demo/diskoptim.R
@@ -1,16 +1,16 @@
 ###############################################################################
-#  Copyright (C) 1994 - 2009, Performance Dynamics Company		      #
-#									      #
-#  This software is licensed as described in the file COPYING, which	      #
-#  you should have received as part of this distribution. The terms	      #
-#  are also available at http://www.perfdynamics.com/Tools/copyright.html.    #
-#									      #
-#  You may opt to use, copy, modify, merge, publish, distribute and/or sell   #
-#  copies of the Software, and permit persons to whom the Software is	      #
-#  furnished to do so, under the terms of the COPYING file.		      #
-#									      #
-#  This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY  #
-#  KIND, either express or implied.					      #
+#  Copyright (c) 1994 - 2013, Performance Dynamics Company
+#
+#  This software is licensed as described in the file COPYING, which
+#  you should have received as part of this distribution. The terms
+#  are also available at http://www.perfdynamics.com/Tools/copyright.html.
+#
+#  You may opt to use, copy, modify, merge, publish, distribute and/or sell
+#  copies of the Software, and permit persons to whom the Software is
+#  furnished to do so, under the terms of the COPYING file.
+#
+#  This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+#  KIND, either express or implied.
 ###############################################################################
 
 #
@@ -22,21 +22,23 @@
 
 require(pdq)
 
-IOinterval     <- 0.006; # seconds
-IOrate	       <- 1 / IOinterval
-fastService   <-  0.005 # seconds
-slowService    <- 0.015 # seconds
+IOinterval  <- 0.006; # seconds
+IOrate	    <- 1 / IOinterval
+fastService <- 0.005 # seconds
+slowService <- 0.015 # seconds
 
 # Start with 3:1 ratio based on speed 15/5 ms
-fastFraction	<- 0.75
+fastFraction <- 0.75
 
-FastDk	       <- "FastDisk"
-SlowDk	      <-  "SlowDisk"
-IOReqsF       <-  "fastIOs"
-IOReqsS      <-   "slowIOs"
+FastDk	<- "FastDisk"
+SlowDk	<- "SlowDisk"
+IOReqsF <- "fastIOs"
+IOReqsS <- "slowIOs"
 
-modelName      <- "Disk I/O Optimization"
+modelName <- "Disk I/O Optimization"
 cat(sprintf("Solving: %s ...\n", modelName))
+
+dkf <- data.frame()
 
 while(fastFraction < 0.86) {
     Init(modelName)
@@ -50,8 +52,8 @@ while(fastFraction < 0.86) {
     SetDemand(FastDk, IOReqsF, fastService)
     SetDemand(FastDk, IOReqsS, 0.0)
 
-    SetDemand(SlowDk, IOReqsS, slowService)
     SetDemand(SlowDk, IOReqsF, 0.0)
+    SetDemand(SlowDk, IOReqsS, slowService)
 
     Solve(CANON)
 
@@ -59,12 +61,30 @@ while(fastFraction < 0.86) {
     sRT  <- GetResponse(TRANS, IOReqsS)
     mRT  <- (fRT * fastFraction) + ((1 - fastFraction) * sRT)
 
-    cat(sprintf("f:  %6.4f, ", fastFraction))
-    cat(sprintf("Uf: %6.4f, ", GetUtilization(FastDk, IOReqsF, TRANS)))
-    cat(sprintf("Us: %6.4f, ", GetUtilization(SlowDk, IOReqsS, TRANS)))
-    cat(sprintf("Rf: %6.4f, ", fRT))
-    cat(sprintf("Rs: %6.4f, ", sRT))
-    cat(sprintf("Rt: %8.6f\n", mRT))
+#    cat(sprintf("f:  %6.4f, ", fastFraction))
+#    cat(sprintf("Uf: %6.4f, ", GetUtilization(FastDk, IOReqsF, TRANS)))
+#    cat(sprintf("Us: %6.4f, ", GetUtilization(SlowDk, IOReqsS, TRANS)))
+#    cat(sprintf("Rf: %6.4f, ", fRT))
+#    cat(sprintf("Rs: %6.4f, ", sRT))
+#    cat(sprintf("Rt: %8.6f\n", mRT))
+
+     dkmets <- c(
+     	fastFraction,
+     	GetUtilization(FastDk, IOReqsF, TRANS),
+     	GetUtilization(SlowDk, IOReqsS, TRANS),
+     	fRT,
+     	sRT,
+     	mRT   
+     )
+     
+     dkf <- rbind(dkf, dkmets)
 
     fastFraction <- fastFraction +  0.01
 }
+
+names(dkf) <- c("f", "Uf", "Us", "Rf", "Rs", "Rt")
+print(dkf)
+cat("\n")
+print("Find the row with minimum RT")
+print(dkf[which(dkf$Rt == min(dkf$Rt)),])
+
