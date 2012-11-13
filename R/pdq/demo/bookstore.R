7e5c9ca0d5d9f86136046df414f41435b1731015
earl-lang <>
2012-11-13 05:41:43 +0000

    Updated to use CreateMultiNode function

--- R/pdq/demo/bookstore.R
+++ R/pdq/demo/bookstore.R
@@ -16,9 +16,12 @@
 #	bookstore.R
 #	
 #	Created by NJG on Wed, Apr 4, 2007
-#       Ported to R by Paul Puglia on Wed Aug 1, 2012
+#   Ported to R by Paul Puglia on Wed Aug 1, 2012
+#	Updated by NJG on Monday, November 12, 2012
 #
-#	PDQ model using 2 MSQ nodes in tandem.
+#	PDQ model using 2 MSQ multi-server nodes in tandem.
+#   Here, prototype MSQ node is replaced by documented CreateMultiNode function.
+#   See http://www.perfdynamics.com/Tools/PDQman.html#tth_sEc3.2
 #	
 #	Big book stores like Barnes & Noble or Borders allow customers to
 #	enter, browse and read books, have coffee, eat, listen to CDs and
@@ -45,34 +48,30 @@
 
 
 
-#       cust per min	
-	arrivalRate  <- 40.0/60.0	
-#       mins
-        browseTime  <- 45.0		
-	buyingTime  <- 	 4.0		
-	cashiers    <-  3.0
+# cust per mins
+arrivalRate  <- 40.0/60.0	
+# times in mins
+browseTime  <- 45.0		
+buyingTime  <-  4.0	
+cashiers    <-  3.0
 		
-	Init("Big Book Store Model")
+Init("Big Book Store Model")
 		
-	streams <- CreateOpen("Customers", arrivalRate);
+CreateOpen("Customers", arrivalRate);
 	
-	SetWUnit("Cust");
-	SetTUnit("Min");	
+# M/M/inf queue defined as 100 times the number of Erlangs = lambda * S
+CreateMultiNode(as.integer(ceiling(arrivalRate * browseTime) * 100), "Browsing", CEN, FCFS)
 	
+# M/M/m where m is the number of cashiers	
+CreateMultiNode(cashiers, "Checkout", CEN, FCFS) 
+	
+# Set service times
+SetDemand("Browsing", "Customers", browseTime)
+SetDemand("Checkout", "Customers", buyingTime)
 
-#	 M/M/inf queue defined as 100 times the number of Erlangs = lambda * S
-	nodes <- CreateNode("Browsing", 
-             as.integer(ceiling(arrivalRate * browseTime) * 100), MSQ);
-	
-#	 M/M/m where m is the number of cashiers	
-	nodes <- CreateNode("Checkout", cashiers, MSQ) 
-	
-# Set service times ...
-	SetDemand("Browsing", "Customers", browseTime);
-	SetDemand("Checkout", "Customers", buyingTime);
-	
-	Solve(CANON);
-	Report();
-   
-
-
+# Change units in Report
+SetWUnit("Cust")
+SetTUnit("Min")	
+		
+Solve(CANON)
+Report()
