f76b15747abc6a9e65e1d95a6511071d9b556f30
Neil J. Gunther <DrQz@users.noreply.github.com>
2018-10-04 10:09:52 -0700

    Added printf for stand-alone output

--- examples/Linux Tech 2007/ebiz_final.pl
+++ examples/Linux Tech 2007/ebiz_final.pl
@@ -1,5 +1,5 @@
-\begin{verbatim}
 #! /usr/bin/perl
+#
 # ebiz_final.pl
 
 use pdq;
@@ -9,26 +9,26 @@ use constant MAXDUMMIES => 12;
 %tpdata   = ( (1,24), (2,48), (4,85), (7,100), (10,99), (20,94) );
 @vusers   = keys(%tpdata);
 
-$model    = "e-Commerce Final Model";
-$work     = "ebiz-tx";
-$node1    = "WebServer";
-$node2    = "AppServer";
-$node3    = "DBMServer";
-$think    = 0.0 * 1e-3;  # same as in test rig
-$dtime    = 2.2 * 1e-3;  # dummy service time 
+$model = "e-Commerce Final Model";
+$work  = "ebiz-tx";
+$node1 = "WebServer";
+$node2 = "AppServer";
+$node3 = "DBMServer";
+$think = 0.0 * 1e-3;  # same as in test rig
+$dtime = 2.2 * 1e-3;  # dummy service time 
 
 # Header for custom report
-printf("%2s\t%4s\t%4s\tD=%2d\n", "N", "Xdat", "Xpdq", MAXDUMMIES);
+printf("Dummmy queues = %4d\n", MAXDUMMIES);
+printf("%2s\t%4s\t%4s\n", "N", "Xdat", "Xpdq");
     
 foreach $users (sort {$a <=> $b} @vusers) {
     pdq::Init($model);
     
-    $pdq::streams = pdq::CreateClosed($work, $pdq::TERM, $users, 
-                        $think);
+    pdq::CreateClosed($work, $pdq::TERM, $users, $think);
     
-    $pdq::nodes = pdq::CreateNode($node1, $pdq::CEN, $pdq::FCFS);
-    $pdq::nodes = pdq::CreateNode($node2, $pdq::CEN, $pdq::FCFS);
-    $pdq::nodes = pdq::CreateNode($node3, $pdq::CEN, $pdq::FCFS);
+    pdq::CreateNode($node1, $pdq::CEN, $pdq::FCFS);
+    pdq::CreateNode($node2, $pdq::CEN, $pdq::FCFS);
+    pdq::CreateNode($node3, $pdq::CEN, $pdq::FCFS);
     
     # Timebase in seconds expressed as milliseconds
     pdq::SetDemand($node1, $work, 8.0 * 1e-3 * ($users ** 0.085));
@@ -38,12 +38,13 @@ foreach $users (sort {$a <=> $b} @vusers) {
     # Create dummy nodes with their service times ... 
     for ($i = 0; $i < MAXDUMMIES; $i++) {
         $dnode = "Dummy" . ($i < 10 ? "0$i" : "$i");
-        $pdq::nodes = pdq::CreateNode($dnode, $pdq::CEN, $pdq::FCFS);
+        pdq::CreateNode($dnode, $pdq::CEN, $pdq::FCFS);
         pdq::SetDemand($dnode, $work, $dtime);
     }
     
-    pdq::Solve($pdq::EXACT);    
-    printf("%2d\t%2d\t%4.2f\n", $users, $tpdata{$users}, 
+    pdq::Solve($pdq::EXACT);
+    
+    printf("%2d\t%4d\t%6.2f\n", $users, $tpdata{$users}, 
             pdq::GetThruput($pdq::TERM, $work));
 }
-\end{verbatim}
+
--- examples/Linux Tech 2007/ebiz_naive.pl
+++ examples/Linux Tech 2007/ebiz_naive.pl
@@ -1,31 +1,32 @@
 #!/usr/bin/perl
-#\begin{verbatim}
-# ebiz.pl
+#
+# ebiz_naive.pl
 
 use pdq;
 
 $users = 20;
-$model    = "e-Commerce Naive Model";
-$work     = "eBiz-tx";
-$node1    = "WebServer";
-$node2    = "AppServer";
-$node3    = "DBMServer";
-$think    = 0.0 * 1e-3;  # treat as free param
+$model = "e-Commerce Naive Model";
+$work  = "eBiz-tx";
+$node1 = "WebServer";
+$node2 = "AppServer";
+$node3 = "DBMServer";
+$think = 0.0 * 1e-3;  # treat as free param
 
 pdq::Init($model);
 
-$pdq::streams = pdq::CreateClosed($work, $pdq::TERM, $users, $think);
-pdq::SetWUnit("Gets");
+pdq::CreateClosed($work, $pdq::TERM, $users, $think);
 
-$pdq::nodes = pdq::CreateNode($node1, $pdq::CEN, $pdq::FCFS);
-$pdq::nodes = pdq::CreateNode($node2, $pdq::CEN, $pdq::FCFS);
-$pdq::nodes = pdq::CreateNode($node3, $pdq::CEN, $pdq::FCFS);
+pdq::CreateNode($node1, $pdq::CEN, $pdq::FCFS);
+pdq::CreateNode($node2, $pdq::CEN, $pdq::FCFS);
+pdq::CreateNode($node3, $pdq::CEN, $pdq::FCFS);
 
 #  Timebase is seconds expressed as milliseconds
 pdq::SetDemand($node1, $work, 9.3 * 1e-3);
 pdq::SetDemand($node2, $work, 2.8 * 1e-3);
 pdq::SetDemand($node3, $work, 0.9 * 1e-3);
 
+pdq::SetWUnit("Gets");
+
 pdq::Solve($pdq::EXACT);
 pdq::Report();
-#\end{verbatim}
+
