47acc75ae8f93d2ebe506f8b357c307def6936ba
Neil J. Gunther <DrQz@users.noreply.github.com>
2018-10-04 21:43:57 -0700

    Updated README

---
+++ examples/Python Models/call-center.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# Standard M/M/m model of call center
+# Created by NJG on Saturday, June 30, 2018
+
+import pdq
+
+agents  = 4     # available to take calls
+aRate   = 0.35  # customers per minute
+sTime   = 10.0  # minutes per customer
+
+pdq.Init("Call Center")
+
+pdq.CreateOpen("Customers", aRate)
+
+pdq.CreateMultiNode(agents, "Agents", pdq.CEN, pdq.FCFS)
+pdq.SetDemand("Agents", "Customers", sTime)
+
+pdq.SetWUnit("Customers")
+pdq.SetTUnit("Minute")
+
+pdq.Solve(pdq.CANON)
+pdq.Report()
+
